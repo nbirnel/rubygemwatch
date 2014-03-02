ece829f9d5534394b618b09657d1b0e53279d05b
Noah Birnel <nbirnel@gmail.com>
2014-03-02 09:48:07 -0800

    remove more junk

--- spec/rubygemwatch_spec.rb
+++ spec/rubygemwatch_spec.rb
@@ -5,14 +5,6 @@ describe RemoteGem do
     @gem = RemoteGem.new('title_case')
   end
 
-  it "should get basic info on the gem" do
-    @gem.basic.is_a?(Hash).should be_true
-  end
-
-  it "should get the name" do
-    @gem.name.should eq "title_case"
-  end
-
   it "should get the versions" do
     @gem.versions.is_a?(Array).should be_true
   end
